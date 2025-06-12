e193e17899e1ff041dc231f2b4a6ccd044a6f795
NyanDesuyo <gerogeroe12@gmail.com>
2025-06-12 15:05:15 +0700

    feat: env variables

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,4 +2,8 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN!;
+export const CLIENT_ID = process.env.CLIENT_ID!;
+export const GUILD_ID = process.env.GUILD_ID!;
+
+export const DATABASE_URL = process.env.DATABASE_URL!;
